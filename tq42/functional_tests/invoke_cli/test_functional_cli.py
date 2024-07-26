e6db173fea28373d82deb5cf3de8cceb97cfe8e0
Steven Beckers <sb@terraquantum.swiss>
2024-07-26 15:40:26 +0200

    fix: fix functional test for invoking cli with normal setup (first of multiple commits)

--- tq42/functional_tests/invoke_cli/test_functional_cli.py
+++ tq42/functional_tests/invoke_cli/test_functional_cli.py
@@ -1,7 +1,5 @@
-import argparse
 import unittest
 
-import tq42.cli.cli_functions as cli
 from tq42.functional_tests.functional_test_config import FunctionalCLITestConfig
 
 
@@ -13,36 +11,24 @@ class TestFunctionalCli(unittest.TestCase, FunctionalCLITestConfig):
         pass
 
     def test_proj_set(self):
-        result = cli.set_project(self.client, self.proj)
-        self.assertIsNotNone(result)
+        result = self.runner.invoke(self.cli_entry, ["proj", "set", self.proj])
+        assert result is not None
 
     def test_proj_show(self):
-        result = cli.proj_show(self.client)
-        success = "org=" in str(result)
-        self.assertEqual(True, success)
+        result = self.runner.invoke(self.cli_entry, ["proj", "show"])
+        assert result.exit_code == 0
+        assert "org=" in result.output
 
     def test_list_orgs(self):
-        result = cli.list_orgs(self.client)
-        success = "org=" in str(result)
-        self.assertEqual(True, success)
+        result = self.runner.invoke(self.cli_entry, ["org", "list"])
+        assert "org=" in result.output
 
     def test_list_proj_by_org(self):
-        result = cli.list_proj_by_org(self.client, self.org)
-        success = "proj=" in str(result)
-        self.assertEqual(True, success)
-
-    def test_list_projects(self):
-        parser = argparse.ArgumentParser()
-
-        parser.add_argument(
-            "--org", type=str, help="Ex: 1df1f520-1834-4d3e-ac1d-65b440fd8f3c"
-        )
-        args = parser.parse_args(["--org", self.org])
-        result = cli.list_proj_by_org(self.client, args.org)
-        success = "proj=" in str(result)
-        self.assertEqual(True, success)
+        result = self.runner.invoke(self.cli_entry, ["proj", "list", "--org", self.org])
+        assert "proj=" in result.output
 
     def test_get_exprun(self):
-        result = cli.get_exprun(self.client, self.exp_run)
-        success = "status" in str(result)
-        self.assertEqual(True, success)
+        result = self.runner.invoke(
+            self.cli_entry, ["exp", "run", "check", self.exp_run]
+        )
+        assert "status" in result.output
