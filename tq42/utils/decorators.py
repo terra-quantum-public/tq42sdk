2f481772d4738306b59c28e1056550ceac4204a4
Mike <ovyan13@gmail.com>
2024-08-27 19:35:08 +0300

    wip: toy e2e tests

---
+++ tq42/utils/decorators.py
@@ -0,0 +1,20 @@
+import functools
+import signal
+
+
+def timeout(seconds):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            def timeout_handler(signum, frame):
+                raise TimeoutError(f"Test timed out after {seconds} seconds")
+
+            signal.signal(signal.SIGALRM, timeout_handler)
+            signal.alarm(seconds)
+            try:
+                result = func(*args, **kwargs)
+            finally:
+                signal.alarm(0)
+            return result
+        return wrapper
+    return decorator
